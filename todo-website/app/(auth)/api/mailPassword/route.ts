373a4abd43084b96f4a358b9fa31455046ed6545
HANZALASALEEM <hanzalabutt903@gmail.com>
2024-08-21 17:01:41 +0500

    Reset Passowrd Mail API

---
+++ todo-website/app/(auth)/api/mailPassword/route.ts
@@ -0,0 +1,59 @@
+import { prisma } from "@/lib/db";
+import { NextResponse } from "next/server";
+import { nanoid } from "nanoid";
+import nodemailer from "nodemailer";
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { email } = body;
+
+  try {
+    const findUser = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+
+    if (findUser) {
+      const token = nanoid(32);
+      const transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
+        },
+      });
+
+      const htmlBody = `Click here to <a href="http://localhost:3000/resetPassword/${token}">Reset Password</a>`;
+      await transport.sendMail({
+        from: '"Maddison Foo Koch 👻" <maddison53@ethereal.email>',
+        to: email,
+        subject: "Password Reset Request",
+        text: `Please reset your password by clicking on the link: http://localhost:3000/resetPassword/${token}`,
+        html: htmlBody,
+      });
+
+      const saveToken = await prisma.user.update({
+        where: {
+          email: email,
+        },
+        data: {
+          token: token,
+        },
+      });
+
+      console.log("Message sent");
+      return NextResponse.json({ msg: "Password reset email sent" });
+    } else {
+      console.log("User not found");
+      return NextResponse.json({ msg: "User not found" }, { status: 404 });
+    }
+  } catch (err) {
+    console.error("Error sending email:", err);
+    return NextResponse.json(
+      { msg: "Internal Server Error", data: err },
+      { status: 500 }
+    );
+  }
+}
